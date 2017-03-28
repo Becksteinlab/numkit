4e3bbf1ca26ad40d25fdee905d608ff88bb301c7
Oliver Beckstein <orbeckst@gmail.com>
2017-03-27 22:44:40 -0700

    moved numkit from GromacsWrapper 0.7.0 to own package
    - extracted history with "git subtree numkit" and reorganized: package is
      now in src/numkit
    - added setup.py (borrowed from datreant.core)
    - added LICENSE (BSD 3-clause, as before)
    - added AUTHORS (based on GromacsWrapper AUTHORS but only numkit contributors)
    - added CHANGES (based on GromacsWrapper CHANGES but only numkit changes)
    - added MANIFEST.in
    - start with release 1.0.0

---
+++ README.rst
@@ -0,0 +1,35 @@
+======
+numkit
+======
+
+.. |docs| |build| |cov|
+
+**numkit** is a small collection of numerical helper functions and
+classes ("numerical first aid kit") that have been useful for
+GromacsWrapper_ and related projects.
+
+Originally, **numkit** was part of GromacsWrapper_ but was split off
+into its own small package in version 0.7.0 of GromacsWrapper.
+
+.. _GromacsWrapper: http://gromacswrapper.readthedocs.org/
+
+
+Getting numkit
+==============
+
+Install from PyPi with ::
+
+    pip install numkit
+
+The source code is available under the BSD 3-clause license (see file
+LICENSE) from GitHub in the repository
+https://github.com/Becksteinlab/numkit ::
+
+  git clone https://github.com/Becksteinlab/numkit
+
+
+Contributing
+============
+
+Contributions are very welcome in all forms, especially through issue
+reports and pull requests.
---
+++ setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for numkit.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='numkit',
+      version='1.0.0',
+      description='numerical first aid kit',
+      author='Oliver Beckstein',
+      author_email='orbeckst@gmail.com',
+      url = 'https://github.com/Becksteinlab/numkit',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      scripts=[],
+      license='BSD',
+      long_description=open('README.rst').read(),
+      tests_require = ['numpy', 'pytest'],
+      install_requires=['numpy', 'scipy']
+)
